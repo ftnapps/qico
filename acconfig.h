81b3da9a9050216e0761343a55b2c42e415ce2ae
Lev Shuvalov <lev@users.sourceforge.net>
2000-10-12 19:38:57 +0000

    Move to autoconf

---
+++ acconfig.h
@@ -0,0 +1,24 @@
+@BOTTOM@
+
+#undef HAVE_LIBCURSES
+
+#undef HAVE_LIBNCURSES
+
+#undef CURS_HAVE_MVVLINE
+
+#define TCP_SPEED 115200 
+
+#define MAX_PATH 256
+
+#define MAX_NODELIST 32
+
+#define MAX_STRING 256
+
+#undef QCC
+
+#undef AMIGA4D
+
+#undef CONFIG
+
+
+
