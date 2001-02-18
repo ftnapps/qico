34532f7786b46143ef362d6f09d323e2106067ec
Lev Shuvalov <lev@users.sourceforge.net>
2001-02-18 11:08:20 +0000

    MFS: Fix stange addresses in queue (don't treat files like 0000ffa6.tut as packets)

--- src/bso.c
+++ src/bso.c
@@ -2,7 +2,7 @@
  * File: bso.c
  * Created at Thu Jul 15 16:10:30 1999 by pk // aaz@ruxy.org.ru
  * bso management
- * $Id: bso.c,v 1.10 2001/02/16 10:43:40 aaz Exp $
+ * $Id: bso.c,v 1.11 2001/02/18 11:08:20 lev Exp $
  **********************************************************/
 #include "headers.h"
 
@@ -53,6 +53,7 @@ int bso_rescan(void (*each)(char *, ftnaddr_t *, int, int ))
 {
 	struct dirent *dez, *den, *dep;
 	char fn[MAX_PATH], *p;
+	int flv;
 	ftnaddr_t a;
 	DIR *dz,*dn,*dp;
 
@@ -81,10 +82,10 @@ int bso_rescan(void (*each)(char *, ftnaddr_t *, int, int ))
 									*p=0;sscanf(dep->d_name+4, "%04hx", &a.p);*p='.';
 									sprintf(fn, "%s/%s/%s/%s", bso_base, dez->d_name,
 											den->d_name, dep->d_name);
-									if(!strcasecmp(p+2, "lo"))
-										each(fn, &a, T_ARCMAIL, bso_flavor(p[1]));	
-									if(!strcasecmp(p+2, "ut"))
-										each(fn, &a, T_NETMAIL, bso_flavor(p[1]));	
+									if(!strcasecmp(p+2, "lo") && F_ERR != (flv = bso_flavor(p[1])))
+										each(fn, &a, T_ARCMAIL, flv);	
+									if(!strcasecmp(p+2, "ut") && F_ERR != (flv = bso_flavor(p[1])))
+										each(fn, &a, T_NETMAIL, flv);	
 									if(!strcasecmp(p+1, "req"))
 										each(fn, &a, T_REQ, F_NORM);	
 								}
