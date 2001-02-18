2c820d884e03c74bb633e67ded0427b83cf3d357
Eugeny Korekin <aaz@users.sourceforge.net>
2001-02-18 11:39:33 +0000

    åÓÌÉ ÎÅ ÓÍÏÇÌÉ ÚÁÐÕÓÔÉÔØ ÐÒÏÇÕ × execnowait, ÔÏ ÔÉÈÏ ÄÏÈÎÅÍ, Á ÎÅ ÐÒÏÄÏÌÖÁÅÍ ÐÏ×ÔÏÒÑÔØ ÄÅÊÓÔ×ÉÑ ÏÓÎÏ×ÎÏÊ ×ÅÔ×É.

--- src/execsh.c
+++ src/execsh.c
@@ -2,7 +2,7 @@
  * File: execsh.c
  * Created at Sun Nov 21 12:05:23 1999 by pk // aaz@ruxy.org.ru
  * Base version of this file was taken from by Eugene Crosser's ifcico 
- * $Id: execsh.c,v 1.4 2000/11/26 13:17:33 lev Exp $
+ * $Id: execsh.c,v 1.5 2001/02/18 11:39:33 aaz Exp $
  ******************************************************************/
 #include "headers.h"
 
@@ -54,6 +54,7 @@ int execnowait(char *cmd,char *p1,char *p2,char *p3)
 		setsid();
 		rc=execl(cmd,cmd,p1,p2,p3,NULL);
 		if(rc<0) write_log("can't exec %s: %s", cmd, strerror(errno));
+		exit(-1);
 	}
 	return 0;
 }
