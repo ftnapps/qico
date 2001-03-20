c44d82c42f68e23ec62db323af1a74800317e786
Lev Shuvalov <lev@users.sourceforge.net>
2001-03-20 19:52:08 +0000

    Fix for previous commit message. Real message is: Split xmem.[ch] into xstr.[ch] and xmem.[ch]. xstr.c doesn't requre write_log().

--- src/xstr.h
+++ src/xstr.h
@@ -2,7 +2,7 @@
  * File: xmem.h
  * Created at Tue Mar 20 22:37:42 2001 by lev // lev@serebryakov.spb.ru
  * 
- * $Id: xstr.h,v 1.1 2001/03/20 19:49:57 lev Exp $
+ * $Id: xstr.h,v 1.2 2001/03/20 19:52:09 lev Exp $
  **********************************************************/
 #ifndef __XSTR_H__
 #define __XSTR_H__
@@ -12,6 +12,7 @@ char *xstrcpy(char *dst, char *src, size_t size);
 #else /* Remeber about "comma operation" */
 #	define xstrcpy(dst,src,size)	(strlcpy((dst),(src),(size)),(dst))
 #endif
+
 #ifndef HAVE_STRLCAT
 char *xstrcat(char *dst, char *src, size_t size);
 #else /* Remeber about "comma operation" */
