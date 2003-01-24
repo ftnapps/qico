70435ad4e661d8f71890135586e944105003ebfe
Cyril Margorin <cyrilm@users.sourceforge.net>
2003-01-24 20:16:57 +0000

    Replace replacement file to its BSD version.

--- src/strcspn.c
+++ src/strcspn.c
@@ -33,22 +33,40 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)strcspn.c	8.1 (Berkeley) 6/4/93";
+#endif /* LIBC_SCCS and not lint */
+#ifndef lint
+static const char rcsid[] =
+  "$FreeBSD: src/lib/libc/string/strcspn.c,v 1.1.1.1.14.1 2001/07/09 23:30:06 obrien Exp $";
+#endif
+
 #include <sys/cdefs.h>
 #include <string.h>
 
 /*
  * Span the complement of string s2.
  */
-size_t strcspn(const char *s1, register const char *s2)
+size_t
+strcspn(s1, s2)
+	const char *s1;
+	register const char *s2;
 {
 	register const char *p, *spanp;
 	register char c, sc;
-	for (p=s1;;) {
-		c=*p++;
-		spanp=s2;
+
+	/*
+	 * Stop as soon as we find any character from s2.  Note that there
+	 * must be a NUL in s2; it suffices to stop when we find that, too.
+	 */
+	for (p = s1;;) {
+		c = *p++;
+		spanp = s2;
 		do {
-			if((sc=*spanp++)==c)
-				return (p-1-s1);
-		} while(sc!=0);
+			if ((sc = *spanp++) == c)
+				return (p - 1 - s1);
+		} while (sc != 0);
 	}
+	/* NOTREACHED */
 }
