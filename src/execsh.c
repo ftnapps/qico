2b6d01f9d9e3c93bbb51d51ad675af97f97ffa2b
Sir Raorn <raorn@users.sourceforge.net>
2003-06-18 20:30:25 +0000

    Error handling added, changed wait(2) to waitpid(2)

--- src/execsh.c
+++ src/execsh.c
@@ -2,24 +2,28 @@
  * File: execsh.c
  * Created at Sun Nov 21 12:05:23 1999 by pk // aaz@ruxy.org.ru
  * Base version of this file was taken from by Eugene Crosser's ifcico 
- * $Id: execsh.c,v 1.7 2001/04/03 19:48:49 lev Exp $
+ * $Id: execsh.c,v 1.8 2003/06/18 20:30:25 raorn Exp $
  ******************************************************************/
 #include "headers.h"
 
 int execsh(char *cmd)
 {
-	int pid,status,rc,sverr;
+	int pid,status,rc;
 
-	if (!(pid=fork())) {
+	if ((pid=fork()) == 0) {
 		to_dev_null();
 		rc=execl(SHELL,"sh","-c",cmd,NULL);
 		exit(-1);
 	}
+	if(pid<0) {
+		write_log("can't fork(): %s",strerror(errno));
+		return -1;
+	}
 	do {
-		rc=wait(&status);sverr=errno;
-	} while (((rc > 0) && (rc != pid)) || ((rc == -1) && (sverr == EINTR)));
+		rc=waitpid(pid, &status, 0);
+	} while ((rc == -1) && (errno == EINTR));
 	if(rc<0) {
-		write_log("wait() returned %d, status %d,%d",rc,status>>8,status&0xff);
+		write_log("error in waitpid(): %s",strerror(errno));
 		return -1;
 	}
 	return WEXITSTATUS(status);
@@ -29,12 +33,16 @@ int execnowait(char *cmd,char *p1,char *p2,char *p3)
 {
 	int pid,rc;
 
-	if (!(pid=fork())) {
+	if ((pid=fork()) == 0) {
 		to_dev_null();
 		setsid();
 		rc=execl(cmd,cmd,p1,p2,p3,NULL);
 		if(rc<0) write_log("can't exec %s: %s", cmd, strerror(errno));
 		exit(-1);
+	}
+	if(pid<0) {
+		write_log("can't fork(): %s",strerror(errno));
+		return -1;
 	}
 	return 0;
 }
