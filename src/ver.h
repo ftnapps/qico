04f60a2589c92777b5541bd655acc6ad8584daf2
Stepan Pologov <sisoft@users.sourceforge.net>
2003-10-05 17:48:56 +0000

    added -v option for all programs. see this for version. change faq about bugs.

--- src/ver.h
+++ src/ver.h
@@ -1,7 +1,10 @@
+/* $Id: ver.h,v 1.3 2003/10/05 17:48:57 sisoft Exp $ */
 #ifndef __VER_H__
 #define __VER_H__
 
 extern char progname[];
 extern char version[];
+extern char cvsdate[];
+extern void u_vers(char *progn);
 
 #endif
