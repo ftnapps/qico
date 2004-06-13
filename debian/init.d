b9e9f3c55a9824e46a047b2d22227a5187a51173
Stepan Pologov <sisoft@users.sourceforge.net>
2004-06-13 17:05:46 +0000

    fix freqs: do not include path in outgoing filenames. fix debian packages install. distrubute stuff/test.pl.

--- debian/init.d
+++ debian/init.d
@@ -1,5 +1,6 @@
 #!/bin/sh
 # initscript for qico.
+# $Id: init.d,v 1.4 2004/06/13 17:05:46 sisoft Exp $
 
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 DAEMON=/usr/sbin/qico
@@ -7,11 +8,17 @@ QCTL=/usr/bin/qctl
 FLAGS="defaults 63"
 
 test -f $DAEMON || exit 0
+if ! test -f /etc/ftn/qico.conf; then
+  if [ "$1" = "start" ]; then
+    echo "Before run qico, rename /etc/ftn/qico.conf.sample to qico.conf and edit it!"
+  fi
+  exit 0
+fi
 
 case "$1" in
   start)
     echo -n "Starting FTN services: qico"
-    start-stop-daemon --start --verbose --exec $DAEMON
+    start-stop-daemon --start --exec $DAEMON
     echo "."
     ;;
   stop)
@@ -19,7 +26,7 @@ case "$1" in
     if [ -f $QCTL ]; then
 	$QCTL -q
     else
-	start-stop-daemon --stop --verbose --exec $DAEMON
+	start-stop-daemon --stop --exec $DAEMON
     fi
     echo "."
     ;;
@@ -27,7 +34,7 @@ case "$1" in
     if [ -f $QCTL ]; then
 	$QCTL -R
     else
-	start-stop-daemon --stop --signal 1 --verbose --exec $DAEMON
+	start-stop-daemon --stop --signal 1 --exec $DAEMON
     fi
     ;;
   restart|force-reload)
@@ -35,10 +42,10 @@ case "$1" in
     if [ -f $QCTL ]; then
 	$QCTL -q
     else
-	start-stop-daemon --stop --verbose --exec $DAEMON
+	start-stop-daemon --stop --exec $DAEMON
     fi
     sleep 1
-    start-stop-daemon --start --verbose --exec $DAEMON
+    start-stop-daemon --start --exec $DAEMON
     echo "."
     ;;
   *)
